a4a483b2d15c04a1d48ae984c4e87491099e342c
Irvin Garcia <irvin.garcia@brin.ai>
2019-02-20 10:17:58 +1100

    ask panel bg color and apply now header changes

--- template-parts/webinar/apply.php
+++ template-parts/webinar/apply.php
@@ -24,7 +24,7 @@
 <section id="form2"  class="module  module--text-background  module--sign-up">
    <div class="container">
         <header class="module__header">
-                  <h2 class="module__title">Become a Member Now</h2>
+                  <h2 class="module__title">Apply Now</h2>
          </header>
         <div class="form-inner">
               <?php echo get_sub_field('form') ?>   
