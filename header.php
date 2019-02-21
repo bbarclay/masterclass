cdd80afefd8be87c17d061db32f4d6c90b77022f
Irvin Garcia <irvin.garcia@brin.ai>
2019-02-21 17:02:23 +1100

    new bb live logo and size adjustment

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 					<div class="grid">
 						<div class="grid__column  grid__column--l-12">
 							<a href="<?php echo home_url(); ?>" title="Return to homepage">
-								<img src="<?php echo get_template_directory_uri(); ?>/images/bb-live.svg" class="logo" width="320">
+								<img src="<?php echo get_template_directory_uri(); ?>/images/bb-live.svg" class="logo">
 							</a>
 						</div>
 					</div>
