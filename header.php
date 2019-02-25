e2955ba080c0a77037b8a5b83d4aaa22835ea8ad
Irvin Garcia <irvin.garcia@brin.ai>
2019-02-25 15:03:19 +1100

    favicon added

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="initial-scale=1, shrink-to-fit=no">
-
+	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" type="image/x-icon" />
 	<?php wp_head(); ?>
 </head>
 
