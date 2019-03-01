c17db4df8fecb61b27bc9687abd7b1260822459e
Irvin Garcia <irvin.garcia@brin.ai>
2019-03-01 14:46:06 +1100

    add input OP input fields

--- template-parts/webinar/video.php
+++ template-parts/webinar/video.php
@@ -3,10 +3,33 @@
         <h1><?php echo get_sub_field('heading') ?></h1>
 
         <div class="video-wrap">
-           <div class="video-inner">
+        	<?php  if ( get_sub_field( 'form_fill' ) ) : ?>
+        	<div id="hideForm" class="input-inner">
+            	<?php echo get_sub_field('form_fill') ?>
+            </div>
+            <?php endif; ?>
+          
+            
+           <?php if ( get_sub_field( 'form_fill' ) ) : ?>
+           <div id="welcomeDiv" class="video-inner" style="display:none;">
+           <?php else: ?>  
+           <div id="welcomeDiv" class="video-inner">
+           <?php endif; ?>
               <?php echo get_sub_field('content') ?>
            </div>
         </div>
 
    </div>
 </section>
+
+<script>
+function showDiv() {
+   document.getElementById('welcomeDiv').style.display = "block";
+   document.getElementById('hideForm').style.display = "none";
+}
+</script>
+
+
+
+
+
