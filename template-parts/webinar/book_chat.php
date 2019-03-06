ecf1b55f7762f40a409601c830d05fcc2a9a5d47
Irvin Garcia <irvin.garcia@brin.ai>
2019-03-06 14:28:34 +1100

    book_chat panel added

---
+++ template-parts/webinar/book_chat.php
@@ -0,0 +1,33 @@
+<section class="myheader">
+<h1>Your Next Step: <span>Option 2</span></h1>
+</section>
+<section class="module apply-now">
+ <div class="container">
+    <div class="row">
+
+       <div class="col-right">
+          <div class="inner-text">
+              <img src="<?php echo get_template_directory_uri() ?>/images/bblogo_v2.svg" width="220" class="bb_logo" />
+             <img src="<?php echo get_template_directory_uri() ?>/images/membership-program.svg" alt="Apply Now" title="Apply Now" />
+             <h3>Let's Build The Business of Your Dreams</span>.</h3>
+
+             <p>Book a chat with one of our team to discover the various options available to you through the Business Blueprint Membership program. During this chat, we will assess your business to ensure that you're a fit for the program, and also gauge your needs and expectations to ensure that we can meet them.</p>
+             
+              <p>We will also provide information on the ways in which we can work together to build the business of your dreams. So go ahead, take the next step now and book in below..</p>
+
+             <a href="#form2" class="button button--large button--wide">Book Now</a>
+          </div>
+       </div>
+    </div>
+ </div>
+</section>
+<section id="form2"  class="module  module--text-background  module--sign-up">
+   <div class="container chat_now_tab">
+        <header class="module__header">
+                  <h2 class="module__title">20 Minute Business<br/> Strategy Session</h2>
+         </header>
+        <div class="form-inner">
+              <?php echo get_sub_field('20_minute_form') ?>   
+        </div>
+   </div>
+</section>
