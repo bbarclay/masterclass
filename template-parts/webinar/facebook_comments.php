78899aeffcc5b5f581937c5315445084235cc590
Irvin Garcia <irvin.garcia@brin.ai>
2019-03-29 10:46:44 +1100

    fb comment panel fix

--- template-parts/webinar/facebook_comments.php
+++ template-parts/webinar/facebook_comments.php
@@ -1,5 +1,5 @@
 <section class="module facebook_comments">
  <div class="container">
- 	<div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-width="100%" data-numposts="5"></div>
+ 	<div class="fb-comments" data-href="https://www.facebook.com/businessblueprint/" data-numposts="5" width="100%"></div>
   </div>
 </section>
