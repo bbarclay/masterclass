870b10b4469ea8a2efcd8124d2a4e15f3aa5d8a1
Irvin Garcia <irvin.garcia@brin.ai>
2019-05-15 10:29:32 +1000

    updated FB comment section

--- template-parts/webinar/facebook_comments.php
+++ template-parts/webinar/facebook_comments.php
@@ -1,5 +1,6 @@
 <section class="module facebook_comments">
  <div class="container">
- 	<div class="fb-comments" data-href="https://www.facebook.com/businessblueprint/" data-numposts="5" width="100%"></div>
+    <?php echo do_shortcode('[gs-fb-comments]'); ?>
+    
   </div>
 </section>
